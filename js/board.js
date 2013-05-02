4e6d57e5f6e54c4d532364b81865e61e19b96e34
Daniel Palma <daniel_palma@yahoo.com>
2013-05-02 15:35:58 -0400

    Starting to fill out the Board class
    Added rowCount and columnCount properties; Added an isInBounds function

--- js/board.js
+++ js/board.js
@@ -1,7 +1,8 @@
-function Board() {
-	this.fubar = "fubar";
+function Board(columns, rows) {
+	this.rowCount = rows;
+	this.columnCount = columns;
 }
 
-Board.prototype.foo = function() {
-	
+Board.prototype.isInBounds = function(col, row) {
+	return (col >= 0) && (col < this.columnCount) && (row >= 0) && (row < this.rowCount);
 };
--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -1,5 +1,31 @@
-describe('Board constructor', function() {
-	it('creates a Board object', function() {
+describe('Board class', function() {
+	
+	it('exists', function() {
 		expect(new Board).toBeDefined();
+	});
+	
+	it('has rowCount and columnCount properties', function() {
+		var board = new Board(10, 12);
+		expect(board.rowCount).toEqual(12);
+		expect(board.columnCount).toEqual(10);
+	});
+	
+	describe('isInBounds function', function() {
+		var board = new Board(10, 15);
+		it('returns true for the center cell', function() {
+			expect(board.isInBounds(5, 7)).toBeTruthy();
+		});
+		it('returns false for a negative row', function() {
+			expect(board.isInBounds(4, -1)).toBeFalsy();
+		});
+		it('returns false for a row too large', function() {
+			expect(board.isInBounds(4, 100)).toBeFalsy();
+		});
+		it('returns false for a negative column', function() {
+			expect(board.isInBounds(-2, 8)).toBeFalsy();
+		});
+		it('returns false for a column too large', function() {
+			expect(board.isInBounds(100, 4)).toBeFalsy();
+		});
 	});
 });
