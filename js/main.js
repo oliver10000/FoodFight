67eabd5f6121e02bb93b29fc37ad1d6ec7b4a341
Daniel Palma <daniel_palma@yahoo.com>
2013-05-07 14:02:01 -0400

    Added EaselJS library support

--- js/main.js
+++ js/main.js
@@ -1,24 +1,15 @@
 var canvas = document.createElement("canvas");
-var context = canvas.getContext("2d");
 canvas.width = 512;
 canvas.height = 480;
 document.body.appendChild(canvas);
 
-var backgroundReady = false;
-var backgroundImage = new Image();
-backgroundImage.onload = function() {
-	backgroundReady = true;
-};
-backgroundImage.src = "images/background.png";
+var stage = new createjs.Stage(canvas);
 
-var render = function() {
-	if (backgroundReady) {
-		context.drawImage(backgroundImage, 0, 0);
-	}
+var background = new createjs.Bitmap("images/background.png");
+stage.addChild(background);
+
+var update = function() {
+	stage.update();
 };
 
-var main = function() {
-	render();
-};
-
-setInterval(main, 1);
+setInterval(update, 1);
