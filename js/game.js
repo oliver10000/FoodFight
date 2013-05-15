4675c9f4fe8023b40b6863086c77205b3607f172
Daniel Palma <daniel_palma@yahoo.com>
2013-05-14 20:59:19 -0400

    Moved board and boardRenderer to Game class

--- js/game.js
+++ js/game.js
@@ -1,5 +1,8 @@
 function Game(canvas) {
 	this.stage = new createjs.Stage(canvas);
+
+	this.board = new Board(6, 12);
+	this.boardRenderer = new BoardRenderer(this.board, this.stage);
 }
 
 Game.prototype.setBackground = function(background) {
--- js/main.js
+++ js/main.js
@@ -7,14 +7,11 @@ var game = new Game(canvas);
 game.setBackground('images/background.png');
 
 var update = function() {
-	var randCol = Math.random() * board.columnCount;
-	var randRow = Math.random() * board.rowCount;
-	board.setCellContents(randCol, randRow, {});
+	var randCol = Math.random() * game.board.columnCount;
+	var randRow = Math.random() * game.board.rowCount;
+	game.board.setCellContents(randCol, randRow, {});
 	game.stage.update();
 };
-
-var board = new Board(6, 12);
-var boardRenderer = new BoardRenderer(board, game.stage);
 
 createjs.Ticker.addEventListener("tick", handleTick);
 
