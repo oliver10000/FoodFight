ed1b55e935c216f1bc0c79b1f14ab914ad5c1d30
Daniel Palma <daniel_palma@yahoo.com>
2013-05-14 21:13:42 -0400

    Create Game object in main.html
    main.js contained only code to create a canvas element and the Game
    object, so this has been moved to a <script> tag in the main page.  The
    canvas element will eventually be declared with a tag so that it can be
    positioned, etc.

--- js/main.js
+++
@@ -1,7 +0,0 @@
-var canvas = document.createElement("canvas");
-canvas.width = 512;
-canvas.height = 480;
-document.body.appendChild(canvas);
-
-var game = new Game(canvas);
-game.setBackground('images/background.png');
